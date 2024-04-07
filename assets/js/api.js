313ee1f6e971f2f1bc6334ef0a5297aabe1b7853
mostafa hamed <154756540+Mostafa7ahmed@users.noreply.github.com>
2024-04-07 22:33:01 +0200

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -14,13 +14,13 @@ export const url = {
         return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
     },
     airPolluation(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
     },
     reverseGeocoding(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}limit=5`;
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}limit=5`;
     },
     Geocoding(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
 
     }
 
